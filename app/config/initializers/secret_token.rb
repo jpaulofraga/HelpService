dbf33baca2e8f39c6f505b7b3824b1c21ceb951a
diegofeijo <dfs.rs@ig.com.br>
2012-11-12 11:26:06 -0200

    Aplicacao Iniciada

---
+++ app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '8997a427d7a07a956c1dc1bd0c98efbcb925079efccd6dbb9ca63446f4242fc35ccad6e2eefb9da8f0dc356e3ca8b328a4383ffb3b03d49e91c9af95703d8b55'
