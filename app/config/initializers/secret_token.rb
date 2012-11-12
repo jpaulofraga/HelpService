44245d80be42deabb1986c79698e535b5796b48f
diegofeijo <dfs.rs@ig.com.br>
2012-11-12 11:21:01 -0200

    removendo

--- app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-App::Application.config.secret_token = '7262d009f78ebfc8f168c2920bec66b9e97062ca2914211efecaaf64a7abd4444cb563b360613e8728f26d902844980f9c5cbece88f39340454a07553b818395'
